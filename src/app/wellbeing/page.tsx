387a4602aed7549b63ccb63246f79f88710ac5e5
osofocarl-web <osofocarl@gmail.com>
2026-05-11 00:30:39 +0100

    fix: src/app/wellbeing/page.tsx

--- src/app/wellbeing/page.tsx
+++ src/app/wellbeing/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { Users, Heart, Coffee, Globe, BookOpen, UserCheck } from "lucide-react";
