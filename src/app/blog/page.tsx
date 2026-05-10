d9e835ee8d0f7c0807173284354ac83a303527ed
osofocarl-web <osofocarl@gmail.com>
2026-05-11 00:30:37 +0100

    fix: src/app/blog/page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import { BookOpen, Calendar, User, ArrowRight } from "lucide-react";
+import { Calendar, User, ArrowRight } from "lucide-react";
 
 const posts = [
   {
